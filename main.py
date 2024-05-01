207da4444d95382aa049d81cc14e0bb6040c267d
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-05-01 14:49:38 -0300

    testando

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ a = 0 # Escolher a unidade
 b = 0 # Escolher a unidade
 
 # Segunda opção
-a = 0 # Escolher a unidade
-k = 0 # Escolher a unidade
-xp = 0 # Escolher a unidade
+A = 0 # em m
+k = 0 # em m
+xp = 0 # em m
 
 # Variaveis de controle
 m = 0
@@ -122,14 +122,15 @@ def simulador():
 
     global frame_entrada, frame_saida, frame_option
     limpar_frames()  # Limpa os frames existentes(pra não gerar varios clones)
-    frame_option = tk.Frame(janela)
-    frame_option.pack()
-    opcao_var = tk.StringVar(frame_option)
-    opcoes = [("Próton", "1"),
-               ("Eletron", "2")]
-    for texto, valor in opcoes:
-         entrada_m = tk.Radiobutton(frame_option, text=texto, variable=opcao_var, value=valor)
-         entrada_m.pack(anchor=tk.W)
+    # frame_option = tk.Frame(janela)
+    # frame_option.pack()
+    # opcao_var = tk.StringVar(frame_option)
+    # opcoes = [("Próton", "1"),
+    #            ("Eletron", "2")]
+    # for texto, valor in opcoes:
+    #      entrada_m = tk.Radiobutton(frame_option, text=texto, variable=opcao_var, value=valor)
+    #      entrada_m.pack(anchor=tk.W)
+    
     # opcao_var = tk.StringVar(frame_option)
     # opcao_var.set("1")  # Valor padrão
 
@@ -144,9 +145,6 @@ def simulador():
     # button.pack()
     # print(m)
 
-    # frame_entrada = tk.Frame(janela)
-    # frame_entrada.pack()
-
     # COLOCAR UM TEXTO DE INTRODUÇÃO AQUI
     #
     # ~ Escrever a introdução ~
@@ -160,22 +158,47 @@ def simulador():
     # Eletron
     #
 
+    # ARRUMAR AQUI
+    # frame_option = tk.Frame(janela)
+    # frame_option.pack()
+    # opcao_var = tk.StringVar(frame_option)
+    # rb_proton = tk.Radiobutton(frame_option, text="Próton", variable=opcao_var, value="1")
+    # rb_eletron = tk.Radiobutton(frame_option, text="Eletron", variable=opcao_var, value="2")
+    # rb_proton.grid(row=0, column=0, padx=10, pady=5)
+    # rb_eletron.grid(row=1, column=0, padx=10, pady=5)
+
+    # frame_entrada = tk.Frame(janela)
+    # frame_entrada.pack()
+    frame_entrada = tk.Frame(janela)
+    frame_entrada.pack()
+
+    m_opcao = tk.StringVar(frame_entrada)
+    m_opcao.set("1")
+
+    rb_proton = tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1")
+
+    rb_eletron = tk.Radiobutton(frame_entrada, text="Eletron", variable=m_opcao, value="2")
+
+    rb_proton.grid(row=0, column=0, padx=10, pady=5)
+    rb_eletron.grid(row=0, column=1, padx=10, pady=5)
+    #
+
     # Criar labels e campos de entrada
     # Caixa = poço de potencial infinito
-    label_valor1 = tk.Label(frame_entrada, text="Largura da caixa (L) em m:")
-    label_valor1.grid(row=0, column=0)
-    entrada_valor1 = tk.Entry(frame_entrada)
-    entrada_valor1.grid(row=0, column=1)
+    label_l = tk.Label(frame_entrada, text="Largura da caixa (L) em m:")
+    label_l.grid(row=0, column=0)
+    entrada_l = tk.Entry(frame_entrada)
+    entrada_l.grid(row=0, column=1)
 
-    label_valor2 = tk.Label(frame_entrada, text="n inicial da particula (Ni):")
-    label_valor2.grid(row=1, column=0)
-    entrada_valor2 = tk.Entry(frame_entrada)
-    entrada_valor2.grid(row=1, column=1)
+    label_ni = tk.Label(frame_entrada, text="n inicial da particula (Ni):")
+    label_ni.grid(row=1, column=0)
+    entrada_ni = tk.Entry(frame_entrada)
+    entrada_ni.grid(row=1, column=1)
 
-    label_valor3 = tk.Label(frame_entrada, text="n final da particula (Nf):")
-    label_valor3.grid(row=2, column=0)
-    entrada_valor3 = tk.Entry(frame_entrada)
-    entrada_valor3.grid(row=2, column=1)
+    label_nf = tk.Label(frame_entrada, text="n final da particula (Nf):")
+    label_nf.grid(row=2, column=0)
+    entrada_nf = tk.Entry(frame_entrada)
+    entrada_nf.grid(row=2, column=1)
 
     # COLOCAR OUTRO TEXTO AQUI
     # 
@@ -184,10 +207,10 @@ def simulador():
     #
 
     # a = inicio do intervalo
-    label_valor4 = tk.Label(frame_entrada, text="a:")
-    label_valor4.grid(row=3, column=0)
-    entrada_valor4 = tk.Entry(frame_entrada)
-    entrada_valor4.grid(row=3, column=1)
+    label_a = tk.Label(frame_entrada, text="a:")
+    label_a.grid(row=3, column=0)
+    entrada_a = tk.Entry(frame_entrada)
+    entrada_a.grid(row=3, column=1)
 
     # b = fim do intervalo
     label_valor5 = tk.Label(frame_entrada, text="b:")
@@ -209,26 +232,7 @@ def simulador():
 
 
 def caixa_1d():
-    def processar_2():
-        global a, k, xp, l, m, n
 
-        print(m)
-        m = m_option(opcao_var.get)
-        print(m)
-        a = float(entrada_a.get())
-        k = float(entrada_k.get())
-        xp = float(entrada_xp.get())
-
-        # Limpar a área de saída antes de exibir a nova saída
-        text_area_saida.delete(1.0, tk.END)
-
-        # Inserir a nova saída na área de texto
-        text_area_saida.insert(tk.END, "Resultados:\n"
-                                        "----------------\n" + str(m) + "\n"
-                                        "a: " + str(a) + "\n"
-                                        "k: " + str(k) + "\n"
-                                        "Posição de x (que multiplique L): " + str(xp) + "\n\n")
-        # ... (Realizar cálculos ou operações com os valores)
 
     global frame_entrada, frame_saida, frame_option
     limpar_frames()  # Limpa os frames existentes(pra não gerar varios clones)
@@ -249,9 +253,6 @@ def caixa_1d():
     # frame_option = tk.Frame(janela)
     # frame_option.pack()
 
-    frame_entrada = tk.Frame(janela)
-    frame_entrada.pack()
-
     # COLOCAR UM OPTIONAL TEXT AQUI
     #
     # Texto: "Qual a particula que deseja analisar? "
@@ -260,13 +261,25 @@ def caixa_1d():
     # Eletron
     #
 
-    frame_option = tk.Frame(janela)
-    frame_option.pack()
-    opcao_var = tk.IntVar()
-    radio_button1 = tk.Radiobutton(frame_option, text="Próton", variable=opcao_var, value=1)
-    radio_button2 = tk.Radiobutton(frame_option, text="Eletron", variable=opcao_var, value=2)
-    radio_button1.grid(row=0, column=0, padx=10, pady=5)
-    radio_button2.grid(row=1, column=0, padx=10, pady=5)
+    # ARRUMAR AQUI
+    # frame_option = tk.Frame(janela)
+    # frame_option.pack()
+
+    frame_entrada = tk.Frame(janela)
+    frame_entrada.pack()
+
+    m_opcao = tk.StringVar(frame_entrada)
+    m_opcao.set("1")
+
+    rb_proton = tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1")
+
+    rb_eletron = tk.Radiobutton(frame_entrada, text="Eletron", variable=m_opcao, value="2")
+
+    rb_proton.grid(row=0, column=0, padx=10, pady=5)
+    rb_eletron.grid(row=0, column=1, padx=10, pady=5)
+
+    #
+
     # for texto, valor in opcoes:
     #      entrada_m = tk.Radiobutton(frame_option, text=texto, variable=opcao_var, value=valor)
     #      entrada_m.pack(anchor=tk.W)
@@ -279,19 +292,19 @@ def caixa_1d():
 
 
     label_a = tk.Label(frame_entrada, text="A em m:")
-    label_a.grid(row=0, column=0)
+    label_a.grid(row=2, column=0)
     entrada_a = tk.Entry(frame_entrada)
-    entrada_a.grid(row=0, column=1)
+    entrada_a.grid(row=2, column=1)
 
     label_k = tk.Label(frame_entrada, text="k em m:")
-    label_k.grid(row=1, column=0)
+    label_k.grid(row=3, column=0)
     entrada_k = tk.Entry(frame_entrada)
-    entrada_k.grid(row=1, column=1)
+    entrada_k.grid(row=3, column=1)
 
     label_xp = tk.Label(frame_entrada, text="Posição de x (que multiplique L):")
-    label_xp.grid(row=2, column=0)
+    label_xp.grid(row=4, column=0)
     entrada_xp = tk.Entry(frame_entrada)
-    entrada_xp.grid(row=2, column=1)
+    entrada_xp.grid(row=4, column=1)
 
     # Criar botão de submit
     botao_submit = tk.Button(frame_entrada, text="Processar", command=lambda: processar_2())
@@ -304,6 +317,32 @@ def caixa_1d():
     # Criar área de texto para exibir a saída
     text_area_saida = tk.Text(frame_saida, width=50, height=10)
     text_area_saida.pack()
+    def processar_2():
+        global a, k, xp, l, m, n
+
+        # Arrumar isso aqui
+        print(m)
+        x = m_opcao.get
+        m = m_option(x)
+        print(m)
+        #
+        a = float(entrada_a.get())
+        k = float(entrada_k.get())
+        xp = float(entrada_xp.get())
+
+        # Limpar a área de saída antes de exibir a nova saída
+        text_area_saida.delete(1.0, tk.END)
+
+        # Inserir a nova saída na área de texto
+        text_area_saida.insert(tk.END, "Entradas:\n"
+                                        "----------------\n" + str(m) + "\n"
+                                        "A: " + str(a) + "\n"
+                                        "k: " + str(k) + "\n"
+                                        "Posição de x (que multiplique L): " + str(xp) + "\n\n"
+                                        "----------------\n"
+                                        "Resultados:\n"
+                                        "----------------\n")
+        # ... (Realizar cálculos ou operações com os valores)
 
 # Janela principal
 menu = tk.Menu(janela)
