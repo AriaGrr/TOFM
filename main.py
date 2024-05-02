c19213980b89fdb5d7cdda04d276f3daf315b06b
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-05-02 18:57:23 -0300

    não funciona main.py

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def labels_and_entries(parent):
     entrada_b.grid(row=8, column=1)
 
 # Funções de interface
-def simulador():
+def simulador(x):
     global m, frame_entrada, frame_saida
     def processar_1():
         global l, ni, nf, a, b, m
@@ -254,8 +254,18 @@ def simulador():
     def simular():
         print("Simulação")
 
+    if (x == 1):
+        limpar_frames()
+        frame_entrada = tk.Frame(janela)
+        frame_saida = tk.Frame(janela) 
+    else:
+        simulador = tk.Tk()
+        simulador.title("Simulador")
+        frame_entrada = tk.Frame(simulador)
+        frame_saida = tk.Frame(simulador)
+        simulador.mainloop()
+    
     m = 1.67 * (10 ** -27)
-    limpar_frames()
     frame_entrada = tk.Frame(janela)
     frame_entrada.pack()
 
@@ -277,101 +287,100 @@ def simulador():
     tk.Button(frame_entrada, text="Processar", command=graficos).grid(row=9, column=2, columnspan=1)
     #
 
-    frame_saida = tk.Frame(janela)
     frame_saida.pack()
     text_area_saida = tk.Text(frame_saida, width=50, height=10)
     text_area_saida.pack()
 
-def simulador_2():
-    global m, frame_entrada, frame_saida
-    def processar_1():
-        global l, ni, nf, a, b, m
-        try:
-            l = float(entrada_l.get())
-            ni = float(entrada_ni.get())
-            nf = float(entrada_nf.get())
-            a = float(entrada_a.get())
-            b = float(entrada_b.get())
+# def simulador_2():
+#     global m, frame_entrada, frame_saida
+#     def processar_1():
+#         global l, ni, nf, a, b, m
+#         try:
+#             l = float(entrada_l.get())
+#             ni = float(entrada_ni.get())
+#             nf = float(entrada_nf.get())
+#             a = float(entrada_a.get())
+#             b = float(entrada_b.get())
 
-            eij = ei_j()
-            eiev = ei_ev()
-            efj = ef_j()
-            efev = ef_ev()
-            E = e_f()
-            lam = lamb()
-            f = freq()
-            vi = vi()
-            vf = vf()
-            ci = ci()
-            cf = cf()
-            ki = ki()
-            kf = kf()
-            area = sqrt(2/l)
-            i = probabilidade(a, b, ni, l)
-            f = probabilidade(a, b, nf, l)
+#             eij = ei_j()
+#             eiev = ei_ev()
+#             efj = ef_j()
+#             efev = ef_ev()
+#             E = e_f()
+#             lam = lamb()
+#             f = freq()
+#             vi = vi()
+#             vf = vf()
+#             ci = ci()
+#             cf = cf()
+#             ki = ki()
+#             kf = kf()
+#             area = sqrt(2/l)
+#             i = probabilidade(a, b, ni, l)
+#             f = probabilidade(a, b, nf, l)
 
-            text_area_saida.delete(1.0, tk.END)
-            text_area_saida.insert(tk.END, f"Resultados:\n----------------\nMassa: {m} kg\n"
-                                           f"Largura da caixa (L): {l} m\n"
-                                           f"n inicial da partícula (Ni): {ni}\n"
-                                           f"n final da partícula (Nf): {nf}\n"
-                                           f"a: {a}\n"
-                                           f"b: {b}\n"f"ψ{ni} (x) = {area:.4e} . sen({(ki):.4e}.x)\n"
-                                  f"ψ{nf} (x) = {area:.4e} . sen({(kf):.4e}.x)\n"
-                                  f"E{ni} = {eij:.4e} J ou {eiev:.4e} eV\n"
-                                  f"E{nf} = {efj:.4e} J ou {efev:.4e} eV\n"
-                                  f"Efoton = {E:.4e} eV\n"
-                                  f"Comprimento de onda do fóton = {lam:.4e} m\n"
-                                  f"Frequência do fóton = {f:.4e} Hz\n"
-                                  f"Velocidade da partícula:\nn = {ni}: v = {vi:.4e} m/s\tn = {nf}: v = {vf:.4e} m/s\n"
-                                  f"Comprimento de onda de De Broglie:\nn = {ni}: ƛ = {ci:.4e} m\tn = {nf}: ƛ = {cf:.4e} m\n"
-                                  f"A probabilidade da partícula estar entre {a:.4e} e {b:.4e} no nível {ni} é de {i*100:.3f} %\n"
-                                  f"A probabilidade da partícula estar entre {a:.4e} e {b:.4e} no nível {nf} é de {f*100:.3f} %\n")
-            # plot_wave_functions(a, b, ni, nf, l)
-            # plot_probability_distribution(a, b, ni, nf, l)
-        except ValueError:
-            messagebox.showerror("Erro de Entrada", "Por favor, insira valores numéricos válidos.")
+#             text_area_saida.delete(1.0, tk.END)
+#             text_area_saida.insert(tk.END, f"Resultados:\n----------------\nMassa: {m} kg\n"
+#                                            f"Largura da caixa (L): {l} m\n"
+#                                            f"n inicial da partícula (Ni): {ni}\n"
+#                                            f"n final da partícula (Nf): {nf}\n"
+#                                            f"a: {a}\n"
+#                                            f"b: {b}\n"f"ψ{ni} (x) = {area:.4e} . sen({(ki):.4e}.x)\n"
+#                                   f"ψ{nf} (x) = {area:.4e} . sen({(kf):.4e}.x)\n"
+#                                   f"E{ni} = {eij:.4e} J ou {eiev:.4e} eV\n"
+#                                   f"E{nf} = {efj:.4e} J ou {efev:.4e} eV\n"
+#                                   f"Efoton = {E:.4e} eV\n"
+#                                   f"Comprimento de onda do fóton = {lam:.4e} m\n"
+#                                   f"Frequência do fóton = {f:.4e} Hz\n"
+#                                   f"Velocidade da partícula:\nn = {ni}: v = {vi:.4e} m/s\tn = {nf}: v = {vf:.4e} m/s\n"
+#                                   f"Comprimento de onda de De Broglie:\nn = {ni}: ƛ = {ci:.4e} m\tn = {nf}: ƛ = {cf:.4e} m\n"
+#                                   f"A probabilidade da partícula estar entre {a:.4e} e {b:.4e} no nível {ni} é de {i*100:.3f} %\n"
+#                                   f"A probabilidade da partícula estar entre {a:.4e} e {b:.4e} no nível {nf} é de {f*100:.3f} %\n")
+#             # plot_wave_functions(a, b, ni, nf, l)
+#             # plot_probability_distribution(a, b, ni, nf, l)
+#         except ValueError:
+#             messagebox.showerror("Erro de Entrada", "Por favor, insira valores numéricos válidos.")
 
-    def graficos():
-        print("Gráficos")
+#     def graficos():
+#         print("Gráficos")
 
-    def simular():
-        print("Simulação")
+#     def simular():
+#         print("Simulação")
 
-    m = 1.67 * (10 ** -27)
+#     m = 1.67 * (10 ** -27)
 
-    simulador = tk.Tk()
-    simulador.title("Simulador")
+#     simulador = tk.Tk()
+#     simulador.title("Simulador")
 
-    frame_entrada = tk.Frame(simulador)
-    frame_entrada.pack()
+#     frame_entrada = tk.Frame(simulador)
+#     frame_entrada.pack()
 
-    tk.Label(frame_entrada, text="Introdução\n").grid(row=0, column=1)
+#     tk.Label(frame_entrada, text="Introdução\n").grid(row=0, column=1)
 
-    m_opcao = tk.StringVar(frame_entrada, "1")
-    m_opcao.trace("w", lambda *args: m_option(m_opcao.get()))
-    tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1").grid(row=2, column=0, padx=10, pady=6)
-    tk.Radiobutton(frame_entrada, text="Elétron", variable=m_opcao, value="2").grid(row=2, column=1, padx=10, pady=5)
-    labels_and_entries(frame_entrada)
+#     m_opcao = tk.StringVar(frame_entrada, "1")
+#     m_opcao.trace("w", lambda *args: m_option(m_opcao.get()))
+#     tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1").grid(row=2, column=0, padx=10, pady=6)
+#     tk.Radiobutton(frame_entrada, text="Elétron", variable=m_opcao, value="2").grid(row=2, column=1, padx=10, pady=5)
+#     labels_and_entries(frame_entrada)
 
-    # Arrumar os botões
-    # Colocar os gráficos
-    tk.Button(frame_entrada, text="Gráficos", command=processar_1).grid(row=9, column=0, columnspan=1)
+#     # Arrumar os botões
+#     # Colocar os gráficos
+#     tk.Button(frame_entrada, text="Gráficos", command=processar_1).grid(row=9, column=0, columnspan=1)
 
-    # Colocar a simulação 
-    tk.Button(frame_entrada, text="Simular", command=simular).grid(row=9, column=1, columnspan=1)
+#     # Colocar a simulação 
+#     tk.Button(frame_entrada, text="Simular", command=simular).grid(row=9, column=1, columnspan=1)
 
-    tk.Button(frame_entrada, text="Processar", command=graficos).grid(row=9, column=2, columnspan=1)
-    #
+#     tk.Button(frame_entrada, text="Processar", command=graficos).grid(row=9, column=2, columnspan=1)
+#     #
 
-    frame_saida = tk.Frame(simulador)
-    frame_saida.pack()
-    text_area_saida = tk.Text(frame_saida, width=50, height=10)
-    text_area_saida.pack()
-    simulador.mainloop()
+#     frame_saida = tk.Frame(simulador)
+#     frame_saida.pack()
+#     text_area_saida = tk.Text(frame_saida, width=50, height=10)
+#     text_area_saida.pack()
+#     simulador.mainloop()
 
 # Função para a caixa 1D
-def caixa_1d():
+def caixa_1d(x):
     global m, frame_entrada, frame_saida
     def processar_2():
         global a, k, xp, l, m, n
@@ -393,9 +402,18 @@ def caixa_1d():
                                            f"Probabilidade de encontrar a partícula na posição {xp}: {p:.3}\n")
         except ValueError as e:
             messagebox.showerror("Erro de Entrada", "Por favor, insira valores numéricos válidos.")
+    if (x == 1):
+        limpar_frames()
+        frame_entrada = tk.Frame(janela)
+        frame_saida = tk.Frame(janela) 
+    else:
+        caixa = tk.Tk()
+        caixa.title("Caixa 1D")
+        frame_entrada = tk.Frame(caixa)
+        frame_saida = tk.Frame(caixa)
+        caixa.mainloop()
+    
     m = 1.67 * (10 ** -27)
-    limpar_frames()
-    frame_entrada = tk.Frame(janela)
     frame_entrada.pack()
 
     tk.Label(frame_entrada, text="Função de onda (no SI) de uma partícula \nconfinada em um poço de \npotencial infinito unidimencional é dada por:").grid(row=0, column=0)
@@ -419,65 +437,64 @@ def caixa_1d():
     entrada_xp.grid(row=4, column=1)
     botao_submit = tk.Button(frame_entrada, text="Processar", command=processar_2)
     botao_submit.grid(row=5, column=0, columnspan=2)
-    frame_saida = tk.Frame(janela)
     frame_saida.pack()
     text_area_saida = tk.Text(frame_saida, width=50, height=10)
     text_area_saida.pack()
 
-def caixa_1d_2():
-    global m, frame_entrada, frame_saida
-    def processar_2():
-        global a, k, xp, l, m, n
-        try:
-            a = float(entrada_a.get())
-            k = float(entrada_k.get())
-            xp = float(entrada_xp.get())
+# def caixa_1d_2():
+#     global m, frame_entrada, frame_saida
+#     def processar_2():
+#         global a, k, xp, l, m, n
+#         try:
+#             a = float(entrada_a.get())
+#             k = float(entrada_k.get())
+#             xp = float(entrada_xp.get())
 
-            l = l_func()
-            n = n_func()
-            p = probalidade_2()
-            # prob = 2 / l * (sin(n * pi * xp) ** 2)
-            text_area_saida.delete(1.0, tk.END)
-            text_area_saida.insert(tk.END, f"Entradas:\n----------------\nMassa: {m} kg\n"
-                                           f"A: {a} m\nk: {k} m^-1\nPosição de x (que multiplique L): {xp}\n"
-                                           "----------------\nResultados:\n----------------\n"
-                                           f"Largura da caixa: {l:.4e} m\n"
-                                           f"Número quântico da partícula: {n}\n"
-                                           f"Probabilidade de encontrar a partícula na posição {xp}: {p:.3}\n")
-        except ValueError as e:
-            messagebox.showerror("Erro de Entrada", "Por favor, insira valores numéricos válidos.")
-    m = 1.67 * (10 ** -27)
-    caixa = tk.Tk()
-    caixa.title("Simulador")
-    frame_entrada = tk.Frame(caixa)
-    frame_entrada.pack()
+#             l = l_func()
+#             n = n_func()
+#             p = probalidade_2()
+#             # prob = 2 / l * (sin(n * pi * xp) ** 2)
+#             text_area_saida.delete(1.0, tk.END)
+#             text_area_saida.insert(tk.END, f"Entradas:\n----------------\nMassa: {m} kg\n"
+#                                            f"A: {a} m\nk: {k} m^-1\nPosição de x (que multiplique L): {xp}\n"
+#                                            "----------------\nResultados:\n----------------\n"
+#                                            f"Largura da caixa: {l:.4e} m\n"
+#                                            f"Número quântico da partícula: {n}\n"
+#                                            f"Probabilidade de encontrar a partícula na posição {xp}: {p:.3}\n")
+#         except ValueError as e:
+#             messagebox.showerror("Erro de Entrada", "Por favor, insira valores numéricos válidos.")
+#     m = 1.67 * (10 ** -27)
+#     caixa = tk.Tk()
+#     caixa.title("Simulador")
+#     frame_entrada = tk.Frame(caixa)
+#     frame_entrada.pack()
 
-    tk.Label(frame_entrada, text="Função de onda (no SI) de uma partícula \nconfinada em um poço de \npotencial infinito unidimencional é dada por:").grid(row=0, column=0)
-    tk.Label(frame_entrada, text=f"\nψ(x) = A sin (k * x) \n").grid(row=0, column=1)
+#     tk.Label(frame_entrada, text="Função de onda (no SI) de uma partícula \nconfinada em um poço de \npotencial infinito unidimencional é dada por:").grid(row=0, column=0)
+#     tk.Label(frame_entrada, text=f"\nψ(x) = A sin (k * x) \n").grid(row=0, column=1)
 
-    m_opcao = tk.StringVar(value="1")
-    m_opcao.trace("w", lambda *args: m_option(m_opcao.get()))
-    tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1").grid(row=1, column=0, padx=10, pady=5)
-    tk.Radiobutton(frame_entrada, text="Elétron", variable=m_opcao, value="2").grid(row=1, column=1, padx=10, pady=5)
-    label_a = tk.Label(frame_entrada, text="A em m:")
-    label_a.grid(row=2, column=0)
-    entrada_a = tk.Entry(frame_entrada)
-    entrada_a.grid(row=2, column=1)
-    label_k = tk.Label(frame_entrada, text="k em m:")
-    label_k.grid(row=3, column=0)
-    entrada_k = tk.Entry(frame_entrada)
-    entrada_k.grid(row=3, column=1)
-    label_xp = tk.Label(frame_entrada, text="Posição de x (que multiplique L):")
-    label_xp.grid(row=4, column=0)
-    entrada_xp = tk.Entry(frame_entrada)
-    entrada_xp.grid(row=4, column=1)
-    botao_submit = tk.Button(frame_entrada, text="Processar", command=processar_2)
-    botao_submit.grid(row=5, column=0, columnspan=2)
-    frame_saida = tk.Frame(caixa)
-    frame_saida.pack()
-    text_area_saida = tk.Text(frame_saida, width=50, height=10)
-    text_area_saida.pack()
-    caixa.mainloop()
+#     m_opcao = tk.StringVar(value="1")
+#     m_opcao.trace("w", lambda *args: m_option(m_opcao.get()))
+#     tk.Radiobutton(frame_entrada, text="Próton", variable=m_opcao, value="1").grid(row=1, column=0, padx=10, pady=5)
+#     tk.Radiobutton(frame_entrada, text="Elétron", variable=m_opcao, value="2").grid(row=1, column=1, padx=10, pady=5)
+#     label_a = tk.Label(frame_entrada, text="A em m:")
+#     label_a.grid(row=2, column=0)
+#     entrada_a = tk.Entry(frame_entrada)
+#     entrada_a.grid(row=2, column=1)
+#     label_k = tk.Label(frame_entrada, text="k em m:")
+#     label_k.grid(row=3, column=0)
+#     entrada_k = tk.Entry(frame_entrada)
+#     entrada_k.grid(row=3, column=1)
+#     label_xp = tk.Label(frame_entrada, text="Posição de x (que multiplique L):")
+#     label_xp.grid(row=4, column=0)
+#     entrada_xp = tk.Entry(frame_entrada)
+#     entrada_xp.grid(row=4, column=1)
+#     botao_submit = tk.Button(frame_entrada, text="Processar", command=processar_2)
+#     botao_submit.grid(row=5, column=0, columnspan=2)
+#     frame_saida = tk.Frame(caixa)
+#     frame_saida.pack()
+#     text_area_saida = tk.Text(frame_saida, width=50, height=10)
+#     text_area_saida.pack()
+#     caixa.mainloop()
 
 def conversor_1():
     def converter_1():
@@ -536,14 +553,14 @@ def main():
     # menu.add_command(label="Conversores", command=conversores)
 
     submenu_simulador = tk.Menu(menu, tearoff=0)
-    submenu_simulador.add_command(label="Normal", command=simulador)
-    submenu_simulador.add_command(label="Pop-Up", command=simulador_2)  # Função lambda simples
+    submenu_simulador.add_command(label="Normal", command=simulador(1))
+    submenu_simulador.add_command(label="Pop-Up", command=simulador(2))  # Função lambda simples
 
     menu.add_cascade(label="Simulador", menu=submenu_simulador)
 
     submenu_caixa = tk.Menu(menu, tearoff=0)
-    submenu_caixa.add_command(label="Normal", command=caixa_1d)
-    submenu_caixa.add_command(label="Pop-Up", command=caixa_1d_2)  # Função lambda simples
+    submenu_caixa.add_command(label="Normal", command=caixa_1d(1))
+    submenu_caixa.add_command(label="Pop-Up", command=caixa_1d(2))  # Função lambda simples
 
     menu.add_cascade(label="Caixa 1D", menu=submenu_caixa)
 
